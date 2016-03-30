4841a5483dfe85ef0df5bc4f93e33b993f3a0a35
AlkalizZ <799478793@qq.com>
2016-03-30 13:38:46 +0800

    添加响应时间，封装jsonp函数

--- script/index.js
+++ script/index.js
@@ -16,26 +16,59 @@
             if(val == oldVal) return;
 
             ul.innerHTML = '';
-            var scripts = document.querySelectorAll('script');
-            if(scripts.length >= 1){
-                body.removeChild(scripts[0]);
-            }
-            var script = document.createElement('script');
-//                script.src = 'http://suggestion.baidu.com/su?wd=我&cb=search';
-            script.src = 'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd='+ val +'&cb=insert';
-            body.appendChild(script);
+            // script.src = 'http://suggestion.baidu.com/su?wd=我&cb=search';
+
+            jsonp({
+                url: 'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd='+ val + '&cb=',
+                time: 3000,
+                callback: function(json){
+                    var frag = document.createDocumentFragment();
+                    for(var i = 0; i < json.s.length; i++){
+                        var li = document.createElement('li');
+                        li.innerHTML = json['s'][i];
+                        frag.appendChild(li);
+                    }
+                    ul.appendChild(frag);
+                },
+                fail: function (mes) {
+                    alert(mes);
+                }
+            });
 
             oldVal = val;
         }, false);
 
-        function insert (json) {
-            for (var i = 0; i < json.s.length; i++) {
-                var li = document.createElement('li');
-                li.innerHTML = json['s'][i];
-                ul.appendChild(li);
+        function jsonp(objects){
+            objects = objects || {};
+            if(!objects.url || !objects.callback){
+                throw new Error('参数不合法');
             }
-            console.log(json);
+
+            //创建script标签并插入
+            var callbackName =  ('jsonp_' + Math.random()).replace(".", "");//随机生成callbackName
+
+            var script = document.createElement('script');
+            var body = document.getElementsByTagName('body')[0];
+            body.appendChild(script);
+
+            window[callbackName] = function (json) {
+                body.removeChild(script);
+                clearTimeout(script.timer);
+                window[callbackName] = null;
+                objects.callback && objects.callback(json);
+            };
+
+            //发出请求
+            script.src = objects.url + callbackName;
+
+            //响应时间
+            if(objects.time){
+                script.timer = setTimeout(function () {
+                    window[callbackName] = null;
+                    body.removeChild(script);
+                    objects.fail && objects.fail('超时');
+                }, objects.time);
+            }
         }
-        window.insert = insert;
     }
 })(window);
